30d7845e7de73d39bf792bd5a11d8ee050b324b3
Vladislav P <varovend@gmail.com>
2021-01-26 13:55:32 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+AOS.init({
+    disable: 'phone',
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+
+
+    offset: 235,
+    delay: 0,
+    duration: 1000,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+
+});
